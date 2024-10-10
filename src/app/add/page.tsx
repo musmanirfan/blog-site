97577b85146e5846da85d5c6f05dba182f5e58b1
musmanirfan <usmanmala356@gmail.com>
2024-10-11 01:03:03 +0500

    changes in add page

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -1,15 +1,18 @@
 "use client"
 
-import { auth, db } from '@/firebase/firebseConfig';
-import { collection } from 'firebase/firestore';
+import { auth, db, storage } from '@/firebase/firebseConfig';
+import { addDoc, collection } from 'firebase/firestore';
 import React, { useState } from 'react'
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { toast } from 'react-toastify';
+import { resolve } from 'path';
 
 export default function Add() {
     const [title, setTitle] = useState("");
     const [file, setFile] = useState<null | File>(null);
     const [tag, setTag] = useState("");
     const [text, setText] = useState("")
+    const [imageURL, setImageURL] = useState("")
 
 
     const add = async () => {
@@ -19,17 +22,64 @@ export default function Add() {
             toast.error("User is not authenticated!");
             return
         }
+
         const collectionRef = collection(db, "blogs")
+
         try {
-            const uploadImg = async () => { 
-                if(!file) return
-                console.log(file);
-                const imgRef = ref(Storage, `uploads/images${Date.now()}-${file.name}`)
-                
-                
+            const metadata = {
+                contentType: 'image/jpeg'
+            };
+            const makeName = (fileName: string): string => {
+                let fileNameArr = fileName.split(".")
+                let lastIndex = fileNameArr.length - 1;
+                let fileExtension = fileNameArr[lastIndex];
+                let newName = `${crypto.randomUUID()}.${fileExtension}`;
+                return newName;
             }
+            const uploadImg = async () => {
+                if (!file) return
+                console.log(file);
+                let newName = makeName(file.name);
+                const storageRef = ref(storage, `images/${newName}`);
+                const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+                uploadTask.on('state_changed',
+                    (snapshot) => {
+                        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        console.log('Upload is ' + progress + '% done');
+                        switch (snapshot.state) {
+                            case 'paused':
+                                console.log('Upload is paused');
+                                break;
+                            case 'running':
+                                console.log('Upload is running');
+                                break;
+                        }
+                    },
+                    (error) => {
+                        switch (error.code) {
+                            case 'storage/unauthorized':
+                                break;
+                            case 'storage/canceled':
+                                break;
+                            case 'storage/unknown':
+                                break;
+                        }
+                    },
+                    async () => {
+                        // Upload completed successfully, now we can get the download URL
+                        const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+                        console.log("File available at", downloadURL);
+                        resolve(downloadURL);
+                    }
+                );
+            }
+        } catch (e) {
+            console.log(e);
+
         }
     }
+
+
     return (
         <div>
             <label htmlFor="title">Title:
--- src/firebase/firebseConfig.ts
+++ src/firebase/firebseConfig.ts
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { GoogleAuthProvider } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 
 
@@ -23,3 +24,5 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 
 export const provider = new GoogleAuthProvider();
+
+export const storage = getStorage(app);
